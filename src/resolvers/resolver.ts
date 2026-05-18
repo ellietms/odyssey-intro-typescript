641468c0df66c72247556cea1aa77625f8e94554
Ellie Tahmasebi <ellie.tms1371@gmail.com>
2026-05-18 17:08:42 +0100

    updated resolver

--- src/resolvers/resolver.ts
+++ src/resolvers/resolver.ts
@@ -2,32 +2,31 @@ import { Resolvers } from "../types";
 
 export const resolvers: Resolvers = {
   Query: {
-    featuredListings: async(_, __, { dataSources }) => {
+    featuredListings: async (_, __, { dataSources }) => {
       return await dataSources.listingAPI.getFeaturedListings();
     },
 
-    getListing: async(_, { id }, { dataSources }) => {
+    getListing: async (_, { id }, { dataSources }) => {
       return await dataSources.listingAPI.getListing(id);
     },
   },
 
   Mutation: {
     createListing: async (_, { newListing }, { dataSources }) => {
-      try{
+      try {
         const listing = await dataSources.listingAPI.createListing(newListing);
-        console.log(">>>", listing)
         return {
           success: true,
           code: 200,
           message: "Listing created successfully",
           listing,
         };
-      } catch(err){
-        return{
+      } catch (err) {
+        return {
           success: false,
           code: 500,
-          message: `Something went wrong: ${err}`
-        }
+          message: `Something went wrong: ${err}`,
+        };
       }
     },
   },
