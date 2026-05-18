ecc14545d5b5a95c8228cc009c5fce1356032472
Ellie Tahmasebi <ellie.tms1371@gmail.com>
2026-05-18 21:55:52 +0100

    changes for main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { ApolloProvider } from "@apollo/client/react";
 
-import {client} from "./apolloClient"
-import {App} from "./App";
+import { client } from "./apolloClient";
+import { App } from "./App";
 
 const root = createRoot(document.getElementById("root")!);
 
 root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-    <App />
+      <App />
     </ApolloProvider>
   </React.StrictMode>,
 );
