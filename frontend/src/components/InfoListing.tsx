fc3f3e580966b22c39c1591b8b7948513f04dda4
Ellie Tahmasebi <ellie.tms1371@gmail.com>
2026-05-18 22:41:49 +0100

    added infoListing

---
+++ frontend/src/components/InfoListing.tsx
@@ -0,0 +1,18 @@
+import type { JSX } from "react";
+import type { Listing } from "../gql/generated";
+
+export const InfoListing = (data: Listing): JSX.Element => {
+  return (
+    <div>
+      <h2> Title: {data.title} </h2>
+      <p> Number of beds: {data.numOfBeds} </p>
+      <p> Cost per night: {data.costPerNight} </p>
+      {data?.amenities.map((eachData) => (
+        <p>
+          {" "}
+          {eachData.name} - {eachData.category}{" "}
+        </p>
+      ))}
+    </div>
+  );
+};
--- frontend/src/gql/generated.ts
+++ frontend/src/gql/generated.ts
@@ -1,93 +1,101 @@
 /** Internal type. DO NOT USE DIRECTLY. */
 type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 /** Internal type. DO NOT USE DIRECTLY. */
-export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
-import { gql } from '@apollo/client';
-import * as Apollo from '@apollo/client';
+export type Incremental<T> =
+  | T
+  | {
+      [P in keyof T]?: P extends " $fragmentName" | "__typename" ? T[P] : never;
+    };
+import { gql } from "@apollo/client";
+import * as Apollo from "@apollo/client";
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
 const defaultOptions = {} as const;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: { input: string; output: string; }
-  String: { input: string; output: string; }
-  Boolean: { input: boolean; output: boolean; }
-  Int: { input: number; output: number; }
-  Float: { input: number; output: number; }
+  ID: { input: string; output: string };
+  String: { input: string; output: string };
+  Boolean: { input: boolean; output: boolean };
+  Int: { input: number; output: number };
+  Float: { input: number; output: number };
 };
 
 export type Amenity = {
-  __typename?: 'Amenity';
-  category: Scalars['String']['output'];
-  id: Scalars['ID']['output'];
-  name: Scalars['String']['output'];
+  __typename?: "Amenity";
+  category: Scalars["String"]["output"];
+  id: Scalars["ID"]["output"];
+  name: Scalars["String"]["output"];
 };
 
 export type CreateListingInput = {
-  amenities: Array<Scalars['ID']['input']>;
-  closedForBookings?: InputMaybe<Scalars['Boolean']['input']>;
-  costPerNight: Scalars['Float']['input'];
-  description: Scalars['String']['input'];
-  numOfBeds: Scalars['Int']['input'];
-  title: Scalars['String']['input'];
+  amenities: Array<Scalars["ID"]["input"]>;
+  closedForBookings?: InputMaybe<Scalars["Boolean"]["input"]>;
+  costPerNight: Scalars["Float"]["input"];
+  description: Scalars["String"]["input"];
+  numOfBeds: Scalars["Int"]["input"];
+  title: Scalars["String"]["input"];
 };
 
 export type CreateListingResponse = {
-  __typename?: 'CreateListingResponse';
-  code: Scalars['Int']['output'];
+  __typename?: "CreateListingResponse";
+  code: Scalars["Int"]["output"];
   listing?: Maybe<Listing>;
-  message: Scalars['String']['output'];
-  success: Scalars['Boolean']['output'];
+  message: Scalars["String"]["output"];
+  success: Scalars["Boolean"]["output"];
 };
 
 export type Listing = {
-  __typename?: 'Listing';
+  __typename?: "Listing";
   amenities: Array<Amenity>;
-  closedForBookings?: Maybe<Scalars['Boolean']['output']>;
-  costPerNight?: Maybe<Scalars['Float']['output']>;
-  id: Scalars['ID']['output'];
-  numOfBeds?: Maybe<Scalars['Int']['output']>;
-  title: Scalars['String']['output'];
+  closedForBookings?: Maybe<Scalars["Boolean"]["output"]>;
+  costPerNight?: Maybe<Scalars["Float"]["output"]>;
+  id: Scalars["ID"]["output"];
+  numOfBeds?: Maybe<Scalars["Int"]["output"]>;
+  title: Scalars["String"]["output"];
 };
 
 export type Mutation = {
-  __typename?: 'Mutation';
+  __typename?: "Mutation";
   createListing: CreateListingResponse;
 };
-
 
 export type MutationCreateListingArgs = {
   newListing?: InputMaybe<CreateListingInput>;
 };
 
 export type Query = {
-  __typename?: 'Query';
+  __typename?: "Query";
   featuredListings: Array<Listing>;
   getListing?: Maybe<Listing>;
 };
 
-
 export type QueryGetListingArgs = {
-  id: Scalars['ID']['input'];
+  id: Scalars["ID"]["input"];
 };
 
-export type GetFeaturedQueryVariables = Exact<{ [key: string]: never; }>;
+export type GetFeaturedQueryVariables = Exact<{ [key: string]: never }>;
 
-
-export type GetFeaturedQuery = { featuredListings: Array<{ id: string, title: string, numOfBeds: number | null, costPerNight: number | null, closedForBookings: boolean | null }> };
-
+export type GetFeaturedQuery = {
+  featuredListings: Array<{
+    id: string;
+    title: string;
+    numOfBeds: number | null;
+    costPerNight: number | null;
+    closedForBookings: boolean | null;
+  }>;
+};
 
 export const GetFeaturedDocument = gql`
-    query GetFeatured {
-  featuredListings {
-    id
-    title
-    numOfBeds
-    costPerNight
-    closedForBookings
+  query GetFeatured {
+    featuredListings {
+      id
+      title
+      numOfBeds
+      costPerNight
+      closedForBookings
+    }
   }
-}
-    `;
+`;
 
 /**
  * __useGetFeaturedQuery__
@@ -104,22 +112,73 @@ export const GetFeaturedDocument = gql`
  *   },
  * });
  */
-export function useGetFeaturedQuery(baseOptions?: Apollo.QueryHookOptions<GetFeaturedQuery, GetFeaturedQueryVariables>) {
-        const options = {...defaultOptions, ...baseOptions}
-        return Apollo.useQuery<GetFeaturedQuery, GetFeaturedQueryVariables>(GetFeaturedDocument, options);
-      }
-export function useGetFeaturedLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetFeaturedQuery, GetFeaturedQueryVariables>) {
-          const options = {...defaultOptions, ...baseOptions}
-          return Apollo.useLazyQuery<GetFeaturedQuery, GetFeaturedQueryVariables>(GetFeaturedDocument, options);
-        }
+export function useGetFeaturedQuery(
+  baseOptions?: Apollo.QueryHookOptions<
+    GetFeaturedQuery,
+    GetFeaturedQueryVariables
+  >,
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useQuery<GetFeaturedQuery, GetFeaturedQueryVariables>(
+    GetFeaturedDocument,
+    options,
+  );
+}
+export function useGetFeaturedLazyQuery(
+  baseOptions?: Apollo.LazyQueryHookOptions<
+    GetFeaturedQuery,
+    GetFeaturedQueryVariables
+  >,
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useLazyQuery<GetFeaturedQuery, GetFeaturedQueryVariables>(
+    GetFeaturedDocument,
+    options,
+  );
+}
 // @ts-ignore
-export function useGetFeaturedSuspenseQuery(baseOptions?: Apollo.SuspenseQueryHookOptions<GetFeaturedQuery, GetFeaturedQueryVariables>): Apollo.UseSuspenseQueryResult<GetFeaturedQuery, GetFeaturedQueryVariables>;
-export function useGetFeaturedSuspenseQuery(baseOptions?: Apollo.SkipToken | Apollo.SuspenseQueryHookOptions<GetFeaturedQuery, GetFeaturedQueryVariables>): Apollo.UseSuspenseQueryResult<GetFeaturedQuery | undefined, GetFeaturedQueryVariables>;
-export function useGetFeaturedSuspenseQuery(baseOptions?: Apollo.SkipToken | Apollo.SuspenseQueryHookOptions<GetFeaturedQuery, GetFeaturedQueryVariables>) {
-          const options = baseOptions === Apollo.skipToken ? baseOptions : {...defaultOptions, ...baseOptions}
-          return Apollo.useSuspenseQuery<GetFeaturedQuery, GetFeaturedQueryVariables>(GetFeaturedDocument, options);
-        }
+export function useGetFeaturedSuspenseQuery(
+  baseOptions?: Apollo.SuspenseQueryHookOptions<
+    GetFeaturedQuery,
+    GetFeaturedQueryVariables
+  >,
+): Apollo.UseSuspenseQueryResult<GetFeaturedQuery, GetFeaturedQueryVariables>;
+export function useGetFeaturedSuspenseQuery(
+  baseOptions?:
+    | Apollo.SkipToken
+    | Apollo.SuspenseQueryHookOptions<
+        GetFeaturedQuery,
+        GetFeaturedQueryVariables
+      >,
+): Apollo.UseSuspenseQueryResult<
+  GetFeaturedQuery | undefined,
+  GetFeaturedQueryVariables
+>;
+export function useGetFeaturedSuspenseQuery(
+  baseOptions?:
+    | Apollo.SkipToken
+    | Apollo.SuspenseQueryHookOptions<
+        GetFeaturedQuery,
+        GetFeaturedQueryVariables
+      >,
+) {
+  const options =
+    baseOptions === Apollo.skipToken
+      ? baseOptions
+      : { ...defaultOptions, ...baseOptions };
+  return Apollo.useSuspenseQuery<GetFeaturedQuery, GetFeaturedQueryVariables>(
+    GetFeaturedDocument,
+    options,
+  );
+}
 export type GetFeaturedQueryHookResult = ReturnType<typeof useGetFeaturedQuery>;
-export type GetFeaturedLazyQueryHookResult = ReturnType<typeof useGetFeaturedLazyQuery>;
-export type GetFeaturedSuspenseQueryHookResult = ReturnType<typeof useGetFeaturedSuspenseQuery>;
-export type GetFeaturedQueryResult = Apollo.QueryResult<GetFeaturedQuery, GetFeaturedQueryVariables>;
+export type GetFeaturedLazyQueryHookResult = ReturnType<
+  typeof useGetFeaturedLazyQuery
+>;
+export type GetFeaturedSuspenseQueryHookResult = ReturnType<
+  typeof useGetFeaturedSuspenseQuery
+>;
+export type GetFeaturedQueryResult = Apollo.QueryResult<
+  GetFeaturedQuery,
+  GetFeaturedQueryVariables
+>;
